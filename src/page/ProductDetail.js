80ecd3f6c6f1966ebec54a6d67227abb69e3b4c6
buzz <chobuzz23@gmail.com>
2023-12-25 11:03:32 +0900

    router setting

---
+++ src/page/ProductDetail.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+const ProductDetail = () => {
+  const { id } = useParams();
+  return <div>상품 상세 페이지{id}</div>;
+};
+
+export default ProductDetail;
